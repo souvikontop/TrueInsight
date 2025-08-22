020ba127473401f0765cab1c4bb40d3ce03a5192
souvikontop <souvikdutta.in@gmail.com>
2025-08-22 23:24:08 +0530

    fixed a bug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
           <svg
             width="28"
             height="28"
-            viewBox="0 0 24 "
+            viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
